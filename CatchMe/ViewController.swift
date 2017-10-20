86c8eff698e2cbc3b4c5bf24964e875387e9305a
三谷昌平 <str.str-g.lor.y-1913@softbank.jp>
2017-10-20 21:02:26 +0900

    Initial Commit

---
+++ CatchMe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CatchMe
+//
+//  Created by 三谷昌平 on 2017/10/20.
+//  Copyright © 2017年 CareQube Developers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
